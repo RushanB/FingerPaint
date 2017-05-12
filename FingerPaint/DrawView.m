e12aae1f76e867383796fc76def831b926d93ed7
RsuhanB <Rushanben@hotmail.com>
2017-05-12 18:31:52 -0400

    Second Commit - Working

--- FingerPaint/DrawView.m
+++ FingerPaint/DrawView.m
@@ -11,16 +11,29 @@
 @implementation DrawView
 
 
--(instancetype)initWithFrame:(CGRect)frame{
+-(instancetype)initWithFrame:(CGRect)frame{ //first path
     self = [super initWithFrame:frame];
     if(self){
         [self setMultipleTouchEnabled:NO];
         [self setBackgroundColor:[UIColor whiteColor]];
         
         _path = [[UIBezierPath alloc]init];
+        _lineColor = [UIColor redColor];
     }
     return self;
 }
+
+
+-(instancetype)initWithCoder:(NSCoder *)coder{ //second path
+    self = [super initWithCoder:coder];
+    if(self) {
+        [self setMultipleTouchEnabled:NO];
+        [self setBackgroundColor:[UIColor whiteColor]];
+        _path = [[UIBezierPath alloc]init];
+    }
+    return self;
+}
+ 
 
 - (void)drawRect:(CGRect)rect {
     [self.lineColor setStroke];  //sets stroke
@@ -32,11 +45,15 @@
 -(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
     UITouch *touch = [touches anyObject];
     CGPoint point = [touch locationInView:self];
-    [self.path moveToPoint:point];  //path returns current point
+    [self.path moveToPoint:point];  //path returns point
 }
 
 -(void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
-    
+    [super touchesBegan:touches withEvent:event];
+    UITouch *touch = [touches anyObject];
+    CGPoint point = [touch locationInView:self];
+    [self.path addLineToPoint:point]; //path returns point
+    [self setNeedsDisplay]; //put to display
 }
 
 @end
--- FingerPaint/ViewController.m
+++ FingerPaint/ViewController.m
@@ -9,21 +9,64 @@
 #import "ViewController.h"
 #import "DrawView.h"
 
-@interface ViewController ()
+@interface ViewController () <UIPickerViewDataSource,UIPickerViewDelegate> //to get color picker methods
 
 @property (strong, nonatomic) IBOutlet DrawView *drawView;
 @property (strong, nonatomic) IBOutlet UITapGestureRecognizer *tapGesture;
 @property (weak, nonatomic) IBOutlet UIPickerView *colorPicker;
-@property (nonatomic) NSArray *colors;
+@property (nonatomic, strong) NSArray *colors;
 
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    [self.view addSubview:self.colorPicker];
-    //[self.colorPicker setHidden:YES];
+    [super viewDidLoad]; //loads view
+    self.colorPicker.delegate = self; //set color delegate
+    [self.view addSubview:self.colorPicker]; //add color picker delegate to view
+    [self.colorPicker setHidden:YES]; //set color picker to hidden
+    self.colors = [[NSArray alloc] initWithObjects:@"Red",@"Green",@"Blue",@"Black",@"Orange",@"Yellow",nil];
+    
+}
+- (IBAction)pressForColor:(id)sender {
+    [self.colorPicker setHidden:NO]; 
+}
+
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
+    return 1; //only holds color
+}
+
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
+    return 6;  //max
+}
+
+-(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{ //fills rows with titles
+    return [self.colors objectAtIndex:row];
+}
+
+-(void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component{//sets color from color picker
+    switch(row) {
+        case 0:
+            self.drawView.lineColor = [UIColor redColor];
+            break;
+        case 1:
+            self.drawView.lineColor = [UIColor greenColor];
+            break;
+        case 2:
+            self.drawView.lineColor = [UIColor blueColor];
+            break;
+        case 3:
+            self.drawView.lineColor = [UIColor blackColor];
+            break;
+        case 4:
+            self.drawView.lineColor = [UIColor orangeColor];
+            break;
+        case 5:
+            self.drawView.lineColor = [UIColor yellowColor];
+            break;
+        default:
+            break;
+    }
 }
 
 
